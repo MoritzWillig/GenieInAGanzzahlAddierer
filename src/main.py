aa88e68552ba4ea29939687ab818640aee680c47
Moritz Willig <moritz.willig@gmail.com>
2017-11-08 15:25:37 +0100

    static file delivery

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+
 
 class Genie(object):
-
     def __init__(self, name):
-        self.app = Flask(name)
+        self.app = Flask(name, static_url_path='')
 
         @self.app.route("/")
-        def hello():
-            return "Hello World!"
+        def serve_index():
+            return send_from_directory('clientside/pages', 'index.html')
+
+        @self.app.route('/content/<path:path>')
+        def serve_static(path):
+            return send_from_directory('content', path)
 
     def serve(self):
         self.app.run()
